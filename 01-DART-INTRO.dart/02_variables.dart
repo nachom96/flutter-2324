5dc828a768af07efbe8fa3f5044bc2670dc2e5c3
Nacho Moreno <nacho_085@hotmail.com>
2023-10-02 20:14:54 +0200

    Variables + dynamic type

---
+++ 01-DART-INTRO.dart/02_variables.dart
@@ -0,0 +1,27 @@
+void main(){
+  
+  const String pokemon = 'Treecko';
+  const int hp = 150;
+  const bool isAlive = true;
+  const List<String> abilities = ['Látigo cepa'];
+  const sprites = <String>['treecko/front.png', 'treecko/battle.png'];
+  
+  // dynamic == null
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1,2,3,4,5,6];
+  errorMessage = { 1,2,3,4,5,6 };
+  errorMessage = () => true;
+  errorMessage = null;
+  
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  $errorMessage
+  """);
+  
+  
+}
