dcda8cf6440fc63ae41c16f9211df5baa1efcf26
Nacho Moreno <nacho_085@hotmail.com>
2024-12-12 17:37:16 +0100

    Update 09_abstract_class.dart

--- 01-DART-INTRO.dart/09_abstract_class.dart
+++ 01-DART-INTRO.dart/09_abstract_class.dart
@@ -1,6 +1,19 @@
 void main() {
   
-  //final windPlant = EnergyPlant();
+  final windPlant = WindPlant( initialEnergy: 100 );
+  
+  print(windPlant);
+  
+  print( 'wind: ${ chargePhone( windPlant )}');
+  
+}
+
+double chargePhone ( EnergyPlant plant ){
+  if (plant.energyLeft < 10 ){
+    throw Exception ('Not enough energy');
+  }
+  
+  return plant.energyLeft -10;
   
 }
 
@@ -17,5 +30,19 @@ abstract class EnergyPlant {
   });
   
   void consumeEnergy( double amount );
+  
+}
+
+// extends o implements
+class WindPlant extends EnergyPlant {
+  
+  WindPlant({ required double initialEnergy })
+      : super ( energyLeft: initialEnergy, type: PlantType.wind);
+  
+  @override
+  void consumeEnergy( double amount ){
+    energyLeft -= amount;
+  }
+  
   
 }
